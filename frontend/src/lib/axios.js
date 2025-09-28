b16780c88db9c68ed283e73fc4cab69864412bee
salartalaa <salarmhmdn@gmail.com>
2025-09-28 18:23:38 +0330

    fix: api url issue

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials: true, // send cookies to the server
+export const axiosInstance = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "/api",
+  withCredentials: true,
 });
-
-export default axiosInstance;
