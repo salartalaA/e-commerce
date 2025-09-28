525b316303b79e3982f54b9381a1d1a481dd6e24
salartalaa <salarmhmdn@gmail.com>
2025-09-28 17:13:01 +0330

    fix: paths

--- backend/server.js
+++ backend/server.js
@@ -34,10 +34,10 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
