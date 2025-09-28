cb96d0095c5c51b71b81f31a576a7ead4f1bd673
salartalaa <salarmhmdn@gmail.com>
2025-09-28 16:47:32 +0330

    fix: path to regexp issue

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use("/api/analytics", analyticsRoutes);
 if (process.env.NODE_ENV === "production") {
 	 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-  app.get("*", (_, res) => {
+  app.get("/*", (_, res) => {
     res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
   });
 }
