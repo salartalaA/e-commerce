cab53a48fd6db8ed9d8473895934dc8bb4f16eea
salartalaa <salarmhmdn@gmail.com>
2025-09-28 17:42:17 +0330

    fix: server.js

--- backend/server.js
+++ backend/server.js
@@ -35,10 +35,10 @@ app.use("/api/analytics", analyticsRoutes);
 
 // make ready for deployment
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("/*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
